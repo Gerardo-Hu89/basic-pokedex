58f8ae3c48569d9f5936fd7a5db28d09d8a59ecd
Gerardo Huerta <gerrdo.hu89@gmail.com>
2023-01-04 19:43:59 -0600

    Test identation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import 'bulma/css/bulma.min.css';
 
 function App() {
-	return (
-		<section className='section'>
-			<div className='box'>
-				<div className='container'>
-					<h1 className='title'>Hello World</h1>
-					<p className='subtitle'>
-						My first website with <strong>Bulma</strong>!
-					</p>
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section className='section'>
+      <div className='box'>
+        <div className='container'>
+          <h1 className='title'>Hello World</h1>
+          <p className='subtitle'>
+            My first website with <strong>Bulma</strong>!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default App;
