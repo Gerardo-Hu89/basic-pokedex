d55734146f4fe386dc5f0f2b499739a9f1abcbc3
Gerardo Huerta <el_gera@Gerardos-MacBook-Pro.local>
2023-01-04 19:18:45 -0600

    pokedex first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<section className='section'>
+			<div className='box'>
+				<div className='container'>
+					<h1 className='title'>Hello World</h1>
+					<p className='subtitle'>
+						My first website with <strong>Bulma</strong>!
+					</p>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default App;
